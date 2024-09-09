a008447d2fb1d9f037a2529d9c1225c9e0ec4bda
Jordan Burger <jordanburger22@gmail.com>
2024-09-09 19:41:01 +0000

    added project

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import githubImg from '../assets/github-mark-white.png';
 import blackPine from '../assets/black-pine.png';
 import junkedOut from '../assets/junked-out.png';
+import leagueSearch from '../assets/league-search.png';
 
 const Portfolio = () => {
     const [hoveredProject, setHoveredProject] = useState(null);
@@ -22,15 +23,15 @@ const Portfolio = () => {
             screenshot: junkedOut,
             appName: 'Junked Out',
             appDescription: "I developed the Junked Out LLC app, a custom web application designed to streamline the process of booking junk removal and small structure demolition services for residential and commercial clients. This project showcases my ability to build functional, user-centric applications with a focus on both front-end development. Form submissions are handled with Netlify's built in form handling.",
-            languagesAndLibraries: ['React', 'Node.js', 'React Bootstrap', 'HTML', 'CSS']
+            languagesAndLibraries: ['React', 'Node.js', 'React Bootstrap', 'HTML', 'CSS', 'React-Router-Dom']
         },
         {
-            githubUrl: 'https://github.com/jordanburger22/assignments',
+            githubUrl: 'https://github.com/jordanburger22/league-data',
             webpageUrl: 'https://github.com/jordanburger22/assignments',
-            screenshot: 'https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            appName: 'Project 3',
-            appDescription: 'A social networking app for connecting people.',
-            languagesAndLibraries: ['React', 'Node.js', 'Express', 'MongoDB', 'HTML', 'CSS']
+            screenshot: leagueSearch,
+            appName: 'League Champions Search Feature Project',
+            appDescription: 'In the League Champions Search Feature Project, I created a foundational web application displaying League of Legends champions, designed to be enhanced by students as part of their learning experience. The project includes a base implementation where students can add a search feature with autocomplete functionality, improving the user interface and experience. The task involves integrating a search bar within the ChampionList component, styling it to fit the existing design, and ensuring that the autocomplete feature activates after three characters. This project serves as a practical exercise for students to practice working with dynamic features, object conversion, and CSS styling, while also providing hands-on experience with filtering data and navigating existing codebases.',
+            languagesAndLibraries: ['React', 'Node.js', 'HTML', 'CSS', 'React-Router-Dom', 'Axios']
         },
         {
             githubUrl: 'https://github.com/jordanburger22/assignments',
